0df4bf4d1b1e64c17841fa18597372db2d795e92
Abdul Afshith <103729934+Afshith@users.noreply.github.com>
2024-03-03 22:29:38 +0530

    Update Day_5_SumOfArrayMinusArrayElement.java

--- src/Day_5_SumOfArrayMinusArrayElement.java
+++ src/Day_5_SumOfArrayMinusArrayElement.java
@@ -3,15 +3,15 @@ import java.util.Arrays;
 
 public class Day_5_SumOfArrayMinusArrayElement {
     public static void main(String[] args) {
-        int[] arr = {10,2,5,6,7,8};
+        int[] arr1 = {10,2,5,6,7,8};
 
         int sum = 0;
-        for(int i:arr){
+        for(int i:arr1){
             sum+=i;
         }
-         for (int i = 0; i < arr.length; i++) {
-            arr[i] = sum-arr[i];
+         for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = sum-arr1[i];
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr1));
     }
 }
